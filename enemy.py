3a91c2ef2b1d851aa5693874a0baa040673fa6a3
Harry Broadbelt <harrybroadbelt@gmail.com>
2024-10-26 15:24:08 +0100

    fweji

--- enemy.py
+++ enemy.py
@@ -16,21 +16,28 @@ class Enemy():
 
     def noticed_player(self, grid, player_location, player_direction):
         wall_present=False
+        print('1')
         if(self.y == player_location[1]): #if enemy and player on same row
+            print('2')
             if(self.x - player_location[0]<0): #enemy is to the left of the player
+                print('3')
                 for i in range(self.x, player_location[0],1): #check to see if a wall exists between the enemy and player
                     if(grid[i][self.y]=='#'):
                         wall_present=True
             else:
+                print('4')
                 for i in range(self.x, player_location[0],-1): #check to see if a wall exists between the enemy and player
                     if(grid[i][self.y]=='#'):
                         wall_present=True
         elif(self.x== player_location[0]): #if enemy and player are on same column
+            print('5')
             if(self.y-player_location[1]<0): #enemy is below the player
+                print('6')
                 for i in range(self.y, player_location[1], 1): #check to see if a wall exists between the enemy and player
                     if(grid[self.x][i]=='#'):
                         wall_present=True
             else:
+                print('7')
                 for i in range(self.y, player_location[1],-1): #check to see if a wall exists between the enemy and player
                     if(grid[self.x][i]=='#'):
                         wall_present=True
