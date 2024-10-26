69e97c2146c29e7a4109c8af2445f4e44dfa190a
mark whittaker <markawhittaker@outlook.com>
2024-10-26 23:44:10 +0100

    sounds update

--- sounds.py
+++ sounds.py
@@ -14,17 +14,35 @@ def distFinder(soundLoc: (int,int), playerLoc: (int,int)) -> int:
     dist = maths.sqrt((soundLoc[0]-playerLoc[0])**2 + (soundLoc[1]-playerLoc[1])**2)
     return dist
 
-def soundVolume(distance: int, primaryDir: Direction) -> (int,int):
+def soundVolume(distance: float, primaryDir: Direction, muffling: int) -> (int,int):
     """Use this with "set_volume()"."""
     if primaryDir.value == 1:
-        voL = 2/distance
-        voR = 1/distance
+        voL = 2/(distance+muffling)
+        voR = 1/(distance+muffling)
     elif primaryDir.value == 2:
-        voL = 1/distance
-        voR = 2/distance
+        voL = 1/(distance+muffling)
+        voR = 2/(distance+muffling)
     else:
-        voL = voR = 1.5/distance
+        voL = voR = 1.5/(distance+muffling)
     return (voL, voR)
+
+def checkWalls(grid,soundLoc,playerLoc):
+    muffling = 0
+    if abs(soundLoc[0]-playerLoc[0]) > abs(soundLoc[1]-playerLoc[1]):
+        if soundLoc[0]-playerLoc[0] < 0:
+            if grid[soundLoc[0]+1][soundLoc[1]] == "#":
+                muffling += 1
+        else:
+            if grid[soundLoc[0]-1][soundLoc[1]] == "#":
+                muffling += 1
+    else:
+        if soundLoc[1]-playerLoc[1] < 0:
+            if grid[soundLoc[0]][soundLoc[1]+1] == "#":
+                muffling += 1
+        else:
+            if grid[soundLoc[0]][soundLoc[1]-1] == "#":
+                muffling += 1
+        
 
 def findSoundDirection(playerLoc: (int,int), playerDirection: Direction, soundLoc: (int, int)) -> Direction:
     xDif = soundLoc[0] - playerLoc[0]
